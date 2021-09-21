97027fb453625d7bcbca0059589c3c39e15e5eae
Ahmed-Alfaouq <mediocre23534@gmail.com>
2021-09-21 19:26:43 -0400

    Add list structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
-import _ from 'lodash';
+import "./style.css";
 
- function component() {
-   const element = document.createElement('div');
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+function component() {
+  const list = [
+    {
+      description: "Finish to do list project",
+      completed: false,
+      index: 0,
+    },
+    {
+      description: "Clean the kitchen",
+      completed: false,
+      index: 0,
+    },
+  ];
+  
+  const ul = document.createElement('ul');
+  ul.className = 'list';
+  const title = document.createElement('h2');
+  title.textContent = "Today's To Do";
+  title.className = 'title'
+  ul.appendChild(title);
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.placeholder = 'Add to your list...';
+  input.className = 'task-input';
+  ul.appendChild(input);
+  const btnSubmit = document.createElement('button');
+  btnSubmit.className = 'btn';
+  btnSubmit.textContent = 'Clear all completed';
 
-   return element;
- }
+  list.map(task => {
+    const li = document.createElement('li');
+    li.id = task.index;
+    li.className = 'list_item';
+    li.innerHTML = `    
+    <input type="checkbox" value=${task.completed}/>
+    <p>${task.description}</p>
+    `;
+    return ul.appendChild(li);
+  });
+  ul.appendChild(btnSubmit);
+  return ul;
+}
 
- document.body.appendChild(component());
+const div = document.getElementById('list-container');
+div.appendChild(component());
